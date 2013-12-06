2189bd88792bcca2d584fb3cbfb573f8168328bb
Cristián Arenas Ulloa <ninoscript@NinoScript.local>
2013-12-06 00:47:02 -0300

    Corrected links to the versions in GitHub (they were previously links to DropBox)

--- watchfile templates/installer.sh
+++ watchfile templates/installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 WATCHFILE=./watchfile.sh
 
-curl -L http://bit.ly/1d048oo -o ${WATCHFILE}
+curl -L http://bit.ly/1f1nlbo -o ${WATCHFILE}
 chmod +x ${WATCHFILE}
 open ${WATCHFILE}
